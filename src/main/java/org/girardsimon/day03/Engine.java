98f70327938d3cfb6d95dbb9bb406a72bcbddf29
Simon Girard <simon.girard1991@gmail.com>
2023-12-04 02:18:54 +0100

    Refacto

--- src/main/java/org/girardsimon/day03/Engine.java
+++ src/main/java/org/girardsimon/day03/Engine.java
@@ -13,26 +13,26 @@ public record Engine(List<List<EngineElement>> rowOfEngineElements) {
     private int sumPartNumbers(int rowIndex) {
         return rowOfEngineElements.get(rowIndex).stream()
                 .filter(engineElement -> isAPartNumber(engineElement, rowIndex))
-                .mapToInt(EngineElement::computeValue)
+                .mapToInt(e -> Integer.parseInt(e.data()))
                 .sum();
     }
     private boolean isAPartNumber(EngineElement engineElement, int rowIndex) {
-        Predicate<EngineElement> isElementAdjacentToAnotherNextRow = isElementAdjacentToAnotherNextRow(engineElement);
-        Predicate<EngineElement> isElementAdjacentToAnotherSameRow = isElementAdjacentToAnotherSameRow(engineElement);
-        return engineElement.computeValue() > 0 &&
+        return engineElement.isANumber() &&
                 IntStream.rangeClosed(Math.max(0, rowIndex - 1), Math.min(rowOfEngineElements.size() - 1, rowIndex + 1))
-                .anyMatch(i -> hasAdjacentInRow(i, i == rowIndex ? isElementAdjacentToAnotherSameRow :
-                        isElementAdjacentToAnotherNextRow));
+                .anyMatch(i -> hasSymbolAdjacent(i, i == rowIndex ? whenSameRow(engineElement) :
+                        whenNeighboringRow(engineElement)));
     }
-    private static Predicate<EngineElement> isElementAdjacentToAnotherSameRow(EngineElement engineElement) {
-        return e -> e.computeXMax() == engineElement.startingColumn()-1 ||
-                e.startingColumn() == engineElement.computeXMax()+1;
+    private static Predicate<EngineElement> whenSameRow(EngineElement element) {
+        return e -> e.computeXMax() == element.startingColumn()-1 ||
+                e.startingColumn() == element.computeXMax()+1;
     }
-    private static Predicate<EngineElement> isElementAdjacentToAnotherNextRow(EngineElement engineElement) {
-        return e -> e.isInColumnsRange(engineElement.startingColumn()-1, engineElement.computeXMax()+1);
+    private static Predicate<EngineElement> whenNeighboringRow(EngineElement element) {
+        return e -> e.isElementInRange(element.startingColumn()-1, element.computeXMax()+1);
     }
-    private boolean hasAdjacentInRow(int rowIndex, Predicate<EngineElement> condition) {
-        return rowOfEngineElements.get(rowIndex).stream().anyMatch(condition);
+    private boolean hasSymbolAdjacent(int rowIndex, Predicate<EngineElement> condition) {
+        return rowOfEngineElements.get(rowIndex).stream()
+                .filter(EngineElement::isASymbol)
+                .anyMatch(condition);
     }
     public int sumGearRatio() {
         return IntStream.range(0, rowOfEngineElements.size())
@@ -42,33 +42,28 @@ public record Engine(List<List<EngineElement>> rowOfEngineElements) {
     private int computeGearRatioForRow(int rowIndex) {
         return rowOfEngineElements.get(rowIndex)
                 .stream()
-                .filter(e -> e.computeValue() == 0)
+                .filter(EngineElement::isASymbol)
                 .mapToInt(e -> computeGearRatioForElement(e, rowIndex))
                 .sum();
     }
     private int computeGearRatioForElement(EngineElement element, int rowIndex) {
-        Predicate<EngineElement> isElementAdjacentToAnotherNextRow = isElementAdjacentToAnotherNextRow(element);
-        Predicate<EngineElement> isElementAdjacentToAnotherSameRow = isElementAdjacentToAnotherSameRow(element);
         List<EngineElement> numbersAdjacent = getNumberAdjacent(rowIndex,
-                isElementAdjacentToAnotherNextRow, isElementAdjacentToAnotherSameRow);
-
-        return numbersAdjacent.size() == 2 ? (numbersAdjacent.getFirst().computeValue() *
-                numbersAdjacent.getLast().computeValue())
+                element);
+        return numbersAdjacent.size() == 2 ? numbersAdjacent.stream().mapToInt(e -> Integer.parseInt(e.data()))
+                .reduce(1, (a,b) -> a*b)
                 : 0;
     }
-    private List<EngineElement> getNumberAdjacent(int rowIndex,
-                                                  Predicate<EngineElement> isElementAdjacentToAnotherNextRow,
-                                                  Predicate<EngineElement> isElementAdjacentToAnotherSameRow) {
+    private List<EngineElement> getNumberAdjacent(int rowIndex, EngineElement element) {
         return IntStream.rangeClosed(Math.max(0, rowIndex - 1), Math.min(rowOfEngineElements.size() - 1, rowIndex + 1))
-                        .mapToObj(i -> numbersInRow(i, i == rowIndex ?
-                                isElementAdjacentToAnotherSameRow : isElementAdjacentToAnotherNextRow))
+                        .mapToObj(i -> numbersAdjacent(i, i == rowIndex ?
+                                whenSameRow(element) : whenNeighboringRow(element)))
                         .flatMap(List::stream)
                         .toList();
     }
-    private List<EngineElement> numbersInRow(int rowIndex, Predicate<EngineElement> condition) {
+    private List<EngineElement> numbersAdjacent(int rowIndex, Predicate<EngineElement> condition) {
         return rowOfEngineElements.get(rowIndex).stream()
+                .filter(EngineElement::isANumber)
                 .filter(condition)
-                .filter(engineElement -> engineElement.computeValue() > 0)
                 .toList();
     }
 }
--- src/main/java/org/girardsimon/day03/EngineElement.java
+++ src/main/java/org/girardsimon/day03/EngineElement.java
@@ -1,13 +1,16 @@
 package org.girardsimon.day03;
 
-public record EngineElement(String data , int startingColumn) {
-    public int computeValue() {
-        return Character.isDigit(data.charAt(0)) ? Integer.parseInt(data) : 0;
-    }
+public record EngineElement(String data, int startingColumn) {
     public int computeXMax() {
         return startingColumn + data.length() -1;
     }
-    public boolean isInColumnsRange(int xMin, int xMax) {
+    public boolean isElementInRange(int xMin, int xMax) {
         return !(startingColumn > xMax || computeXMax() < xMin);
+    }
+    public boolean isANumber() {
+        return Character.isDigit(data.charAt(0));
+    }
+    public boolean isASymbol() {
+        return data.length() == 1;
     }
 }
